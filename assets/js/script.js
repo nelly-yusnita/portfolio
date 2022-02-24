225fd40cd1ec5281b1f2abb65cf59a73ad5442ec
nelly-yusnita <yusnitaanelly@gmail.com>
2022-02-24 14:23:23 +0700

    delete scrollup

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,15 +60,6 @@ portfolioButtonLink.forEach((bc) =>{
 })
 // ===== end portfolio ===== //
 
-// ===== SHOW SCROLL UP ===== //
-function scrollUp(){
-	const scrollUp = document.getElementById('scroll-up');
-	
-	if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
-}
-window.addEventListener('scroll', scrollUp);
-// ===== end show scroll up ===== //
-
 // ===== DARK LIGHT THEME ===== //
 const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme';
