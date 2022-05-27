e3da231004887721fff638956de71627982496aa
undang-digital-aja <undangdigitalaja@gmail.com>
2022-05-27 10:57:00 +0700

    revisi template

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,87 @@ portfolioButtonLink.forEach((bc) =>{
 })
 // ===== end portfolio ===== //
 
+// ===== LANGUAGES ===== //
+const navLanguages = document.querySelector('.nav__languages'),
+		languageButton = document.querySelectorAll('.language__button');
+
+// 1. Home Section
+const profileGreeting = document.querySelector('.profile__greeting');
+
+// 2. About Section
+const aboutDescription = document.querySelector('.about__description');
+
+// 3. Contact Section
+const contactDescription = document.querySelector('.contact__description');
+
+languageButton.forEach(el =>{
+	el.addEventListener('click', ()=>{
+		navLanguages.querySelector('.active-language').classList.remove('active-language');
+		el.classList.add('active-language');
+
+		const attr = el.getAttribute('language');
+		// 1. Home Section
+		profileGreeting.textContent = data[attr].profile__greeting;
+
+		// 2. About Section
+		aboutDescription.textContent = data[attr].about__description;
+
+		// 3. Contact Section
+		contactDescription.textContent = data[attr].contact__description;
+	});
+});
+
+var data = {
+	// ENGLISH //
+	"english":{
+		// 1. Home Section
+		"profile__greeting" : "Hello, I'm",
+
+		// 2. About Section
+		"about__description" : "Hey! My Name is Nelly Yusnita. I'm a Freelance Junior Frontend Developer based in Indonesia. I love working on fun little projects especially for individuals and small businesses during my free time. I provide custom designs at afforable prices. Frequently your website is the first impression your customers will get, so make sure it’s a good one. Since my time is split between a few different things, I’m not able to take on every project I’d like to, but I'm always looking for fun work. Take a look at my portfolio below, if you think I'd be a good match send me an email or message on my social media.",
+
+		// 3. Contact Section
+		"contact__description" : "If you have a project that you want to carry out, do not hesitate and tell me what it is, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you."
+	},
+	// end english //
+
+	// INDONESIA //
+	"indonesia":{
+		// 1. Home Section
+		"profile__greeting" : "Halo, Saya",
+
+		// 2. About Section
+		"about__description" : "Hai, nama saya Nelly Yusnita. Saya seorang freelance Frontend Developer dari Indonesia. Saya suka mengerjakan proyek kecil yang menyenangkan terutama untuk individu dan bisnis kecil di waktu luang saya. Saya menyediakan desain khusus dengan harga terjangkau. Seringkali website Anda adalah kesan pertama yang akan didapatkan pelanggan Anda, jadi pastikan Anda punya website yang bagus. Karena waktu saya terbagi antara beberapa hal yang berbeda, saya tidak dapat mengerjakan setiap proyek yang saya inginkan, tetapi saya selalu mencari pekerjaan yang menyenangkan. Lihatlah portofolio saya di bawah ini, jika menurut Anda saya cocok, kirimkan saya email atau pesan di media sosial saya.",
+
+		// 3. Contact Section
+		"contact__description" : "Jika Anda memiliki proyek yang ingin Anda laksanakan, jangan ragu dan beri tahu saya apa itu, kotak masuk saya selalu terbuka. Apakah Anda memiliki pertanyaan atau hanya ingin menyapa, saya akan mencoba yang terbaik untuk menghubungi Anda kembali."
+	}
+	// end indonesia //
+}
+// ===== end languages ===== //
+
+// ===== POP UP ===== //
+const downloadButton = document.querySelector('.download__button'),
+		popup = document.querySelector('.popup')
+
+downloadButton.addEventListener('click', ()=>{
+	popup.style.display = 'flex';
+
+	setTimeout(() =>{
+		popup.style.display = 'none';
+	}, 2500)
+})
+// ===== end pop up ===== //
+
+// ===== SCROLL UP ===== //
+function scrollUp(){
+	const scrollUp = document.getElementById('scroll-up');
+
+	if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp);
+// ===== end scroll up ===== //
+
 // ===== DARK LIGHT THEME ===== //
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
@@ -85,4 +166,29 @@ themeButton.addEventListener('click', () =>{
 	localStorage.setItem('selected-icon', getCurrentIcon())
 })
 // ===== end dark light theme ===== //
+
+// ===== SCROLL REVEAL ANIMATION ===== //
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
+	// reset: true
+})
+
+sr.reveal(`.profile__border, .section__subtitle, .about__img, .contact__description`)
+sr.reveal(`.section__title, .contact__button`, {delay: 150})
+
+// Home //
+sr.reveal(`.profile__greeting`, {delay: 150})
+sr.reveal(`.profile__name`, {delay: 200})
+sr.reveal(`.profile__profession`, {delay: 250})
+sr.reveal(`.profile__buttons`, {delay: 300})
+sr.reveal(`.home__social`, {delay: 350, origin: 'left'})
+sr.reveal(`.home__scroll`, {delay: 350, origin: 'right'})
+
+// About //
+sr.reveal(`.social__media-link, .portfolio__card`, {interval: 100, origin: 'top'})
+sr.reveal(`.about__description`, {delay: 150})
+// ===== end scroll reveal animation ===== //
 
